3394cc7527ed2daf716a349c1b2787706aa5cdb5
ilovehleb <ilovehleb@gmail.com>
2022-08-05 22:38:55 +0300

    сделал задания на семинаре

--- examples/example002_sum/Program.cs
+++ examples/example002_sum/Program.cs
@@ -1,6 +1,36 @@
-﻿int numberA = new Random().Next(1,10000);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10000);
-Console.WriteLine(numberB);
-int sum = numberA + numberB;
-Console.Write(sum);
+﻿Console.WriteLine("Введите цифру дня недели");
+
+int number=int.Parse (Console.ReadLine());
+
+if (number == 1)
+{
+    Console.Write("Это понедельник.");
+}
+else if (number == 2)
+{
+    Console.Write("Это вторник.");
+}
+else if (number == 3)
+{
+    Console.Write("Это среда.");
+}
+else if (number == 4)
+{
+    Console.Write("Это четверг.");
+}
+else if (number == 5)
+{
+    Console.Write("Это пятница.");
+}
+else if (number == 6)
+{
+    Console.Write("Это суббота.");
+}
+else if (number == 7)
+{
+    Console.Write("Это воскресенье.");
+}
+else
+{
+  Console.Write("Такого дня недели не существует))");
+}
---
+++ examples/example003_div/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+
+int numberA=int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int numberB=int.Parse (Console.ReadLine());
+
+if (numberA == numberB*numberB)
+{
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ является квадратом второго.");   
+}
