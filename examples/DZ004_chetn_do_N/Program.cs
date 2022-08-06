f96f49d7f7d447ccc22dbb8abda9fb88d8ac6b11
ilovehleb <ilovehleb@gmail.com>
2022-08-06 14:45:45 +0300

    Четные числа от 1 до N

---
+++ examples/DZ004_chetn_do_N/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N - ");
+int N = int.Parse (Console.ReadLine());
+int count = 2;
+Console.WriteLine("Четные числа от 1 до N :");
+while (count < N)
+{
+    if (count%2 == 0)
+    {
+        Console.WriteLine(count);
+        count += 2;
+
+    }
+}
