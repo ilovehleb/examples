2e1778009ca2500cf8a013ba55762bc7052387d2
ilovehleb <ilovehleb@gmail.com>
2022-08-06 14:01:30 +0300

    Определение чётности числа

---
+++ examples/DZ003_chetn_chislo/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число - ");
+int x = int.Parse (Console.ReadLine());
+if (x%2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число НЕ является четным");
+}
