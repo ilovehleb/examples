fbc254feddf89b10293d2516af259061546c48d3
ilovehleb <ilovehleb@gmail.com>
2022-08-06 13:51:59 +0300

    исправление фалов

---
+++ examples/DZ001_max&min/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число:  ");
+int numA=int.Parse (Console.ReadLine());
+Console.WriteLine("Введите второе число:  ");
+int numB=int.Parse (Console.ReadLine());
+if (numA > numB)
+{
+    Console.WriteLine("Наибольшее число - " + numA + "   Наименьшее число - " + numB);
+
+}
+else if (numA < numB)
+{
+    Console.WriteLine("Наибольшее число - " + numB + "   Наименьшее число - " + numA);
+}
+else
+{
+    Console.WriteLine("Они равны");
+}
---
+++ examples/DZ002_3int&max/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число:  ");
+int nA=int.Parse (Console.ReadLine());
+Console.WriteLine("Введите второе число:  ");
+int nB=int.Parse (Console.ReadLine());
+Console.WriteLine("Введите третье число:  ");
+int nC=int.Parse (Console.ReadLine());
+if (nA >= nB && nA >= nC)
+{
+    Console.WriteLine("Максимальное число - " + nA);
+}
+else if (nB >= nA && nB >= nC)
+{
+    Console.WriteLine("Максимальное число - " + nB);
+}
+else
+{
+    Console.WriteLine("Максимальное число - " + nC);
+}
