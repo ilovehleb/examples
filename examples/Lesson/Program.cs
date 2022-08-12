d7e1119a7d4c5095f5ad31dedfd6d069e815249e
ilovehleb <ilovehleb@gmail.com>
2022-08-12 11:26:11 +0300

    пояснения в лессон про строку и массив

--- examples/Lesson/Program.cs
+++ examples/Lesson/Program.cs
@@ -1,16 +1,5 @@
-﻿//Четвертая задача. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.//
-
-
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine());
-int count = 2;
-
-while (count < number)
-{
-    Console.WriteLine(count);
-    count += 2;
-}
-
+﻿string stroka = "abcd"; // char[] stroka = new char[] {"a","b","c","d"} (строка есть массив)
+Console.WriteLine(stroka[2]);// можно выводить отдельные символы из строки указывая их индекс.
 
 
 
