77e18c0e3cfc2489c80f228c9b280c5a96f3b947
ilovehleb <ilovehleb@gmail.com>
2022-08-12 11:44:47 +0300

    второе ДЗ второго семинара

---
+++ examples/DZ002_2/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите любое число : ");
+string stroka = Console.ReadLine();
+if (stroka.Length>=3){Console.WriteLine(stroka[2]);}
+else {Console.WriteLine("Третьей цифры нет.");}
