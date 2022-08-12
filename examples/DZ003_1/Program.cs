848675ba63b4bfc6f7d4b7c940128abf7b26c0e9
ilovehleb <ilovehleb@gmail.com>
2022-08-12 22:14:17 +0300

    первая задача ДЗ третьего семинара

---
+++ examples/DZ003_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("введите число");
+string num = Console.ReadLine();
+string rev = string.Concat(num.Reverse());
+if (num == rev){Console.WriteLine("палиндром");}
+else {Console.WriteLine("не палиндром");}
--- examples/Lesson/Program.cs
+++ examples/Lesson/Program.cs
@@ -1,7 +1,6 @@
-﻿string stroka = "abcd"; // char[] stroka = new char[] {"a","b","c","d"} (строка есть массив)
-Console.WriteLine(stroka[2]);// можно выводить отдельные символы из строки указывая их индекс.
+﻿//string stroka = "abcd"; // char[] stroka = new char[] {"a","b","c","d"} (строка есть массив)
+//Console.WriteLine(stroka[2]);// можно выводить отдельные символы из строки указывая их индекс.
 
 
-
-
+//int num = New Random().Next(0,100) // выводим рандомное число от 0 до 99
 
