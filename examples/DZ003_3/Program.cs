93f0de56660e7c7003c3f7bcc4cd280fc0175e82
ilovehleb <ilovehleb@gmail.com>
2022-08-13 12:01:17 +0300

    третье ДЗ третьего семинара

---
+++ examples/DZ003_3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число N : ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+while (count<=N) {Console.Write((count*count*count)+"  "); count++;}
+
