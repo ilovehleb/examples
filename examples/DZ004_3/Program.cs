2c491a002646f2493ef3d87bb5b51f90d90da1ac
ilovehleb <ilovehleb@gmail.com>
2022-08-14 15:19:58 +0300

    третье ДЗ четвертого семинара

--- examples/DZ004_3/Program.cs
+++ examples/DZ004_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+int size = 8; //можно менять количество символов в массиве.
+int i = 0;
+
+int[] arr = new int[size];
+Random Ran = new Random();
+
+for (i = 0; i < size; i++)
+{
+    arr[i] = Ran.Next(0, 10);
+    Console.Write($"{arr[i]} ");
+}
