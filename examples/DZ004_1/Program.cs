fbd04173cfba4fea80716d0d4a10c06f4b6c98c0
ilovehleb <ilovehleb@gmail.com>
2022-08-14 13:20:10 +0300

    второе ДЗ четвертой лекции + новые папки лессон

---
+++ examples/DZ004_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+while (true)
+{
+int a;
+int b;
+double x;
+Console.Write("Введите первое число :  ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число :  ");
+b = int.Parse(Console.ReadLine());
+x = Math.Pow(a,b);
+Console.WriteLine(a + " в степени " + b +" = " + x);
+}
---
+++ examples/DZ004_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите лююбое целое число:  ");
+string xi = Console.ReadLine();
+int count = xi.Length;
+int x = int.Parse (xi);
+int i = 0;
+int sum = 0;
+Console.WriteLine("Количество знаков в числе - " + count + ".");
+
+while (i < count)
+            {
+                sum += x % 10;
+                x = x /10 ;
+                i++;
+            }
+Console.Write("Сумма всех чисел в числе '" + xi + "' равна - " + sum + ".");
