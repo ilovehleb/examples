1710c762f6d2360e5c43879736a7d8be7e52a548
ilovehleb <ilovehleb@gmail.com>
2022-08-15 21:15:31 +0300

    qwe

--- examples/Lesson/Program.cs
+++ examples/Lesson/Program.cs
@@ -4,3 +4,14 @@
 
 //int num = New Random().Next(0,100) // выводим рандомное число от 0 до 99
 
+//int[] array = new int[] {1,2,3,4,5,6,7};
+Console.WriteLine("Введите число :   ");
+int num = int.Parse(Console.ReadLine());
+if (num == 6 || num == 7)
+{
+Console.WriteLine("Выходной");
+}
+else
+{
+Console.WriteLine("Рабочий день");
+}
