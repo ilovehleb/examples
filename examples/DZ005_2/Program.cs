40657aed1f7c8da8a53610d891ef48f0c69e8db7
ilovehleb <ilovehleb@gmail.com>
2022-08-20 22:09:05 +0300

    второе ДЗ пятого семинара

---
+++ examples/DZ005_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+int[] arr(int length, int min = 0, int max = 10)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
+
+int sum(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        result += array[i];
+    }
+    return result;
+}
+
+Console.Write("Введите длину массива:");
+int x = int.Parse(Console.ReadLine());
+int[] array = arr(x);
+Console.Write("Mассив: ");
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.Write("Сумма элементов: ");
+Console.WriteLine(sum(array));
