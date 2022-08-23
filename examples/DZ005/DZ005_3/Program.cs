ab36331b77710a71183f538c73bd388e854747a5
ilovehleb <ilovehleb@gmail.com>
2022-08-23 11:36:18 +0300

    третье ДЗ пятого семинара

--- examples/DZ005/DZ005_3/Program.cs
+++ examples/DZ005/DZ005_3/Program.cs
@@ -1,3 +1,37 @@
-﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
 
 // [3 7 22 2 78] -> 76
+
+int[] Arr(int length = 10, int min = 0, int max = 10)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
+
+string PrintArray(int[] array)
+{
+    return (String.Join(", ", array));
+}
+int raznica(int[] array)
+{
+    int mini = 0;
+    int maxi = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > array[maxi])
+            maxi = i;
+        if (array[i] < array[mini])
+            mini = i;
+    }
+    return array[maxi] - array[mini];
+}
+
+int[] array = Arr(15,-100,100);
+Console.Write("Mассив: ");
+Console.WriteLine(PrintArray(array));
+Console.Write("Разница между максимальным и минимальным числом составляет: ");
+Console.WriteLine(raznica(array));
---
+++ examples/Lesson/Lesson4_3/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Clear();
+double Fibonacci(int n)
+{
+    if (n == 0 || n == 1) return 1;
+    else return (Fibonacci(n - 1) + Fibonacci(n - 2));
+}
+for (int i = 0; i < 42; i++)
+{
+    Console.WriteLine($"F({i}) = {Fibonacci(i)}");
+}
