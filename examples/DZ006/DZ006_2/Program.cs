ff62977048256e0aceaeb79623ae39082440a533
ilovehleb <ilovehleb@gmail.com>
2022-08-24 20:23:52 +0300

    второе ДЗ шестого семинара

--- examples/DZ006/DZ006_2/Program.cs
+++ examples/DZ006/DZ006_2/Program.cs
@@ -1,4 +1,24 @@
-﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void tochka(double k1, double b1,double k2, double b2)
+{
+    double x;
+    double y;
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+    Console.WriteLine("x=" + x +" y=" + y);
+}
+
+Console.WriteLine("Введите первую координату (b1)");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите первую координату (k1)");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите первую координату (b2)");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите первую координату (k2)");
+double k2 = double.Parse(Console.ReadLine());
+tochka(k1,b1,k2,b2);
 
