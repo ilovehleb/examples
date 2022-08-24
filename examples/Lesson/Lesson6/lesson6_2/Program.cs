c95c48ec8245c516709728e57a40e80cecee0e2f
ilovehleb <ilovehleb@gmail.com>
2022-08-24 18:09:12 +0300

    задача 2 из 6 лекции

--- examples/Lesson/Lesson6/lesson6_2/Program.cs
+++ examples/Lesson/Lesson6/lesson6_2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2. Вычислить среднее арифметическое положительных элементов в одномерном массиве. 
+// Элементы массива задаются случано и лежат в промежутке от -10 до 10
+
+// [1, -5, 8, 4, -9] -> 4.33
+
+int[] fillarray(int capacity)
+{
+    int[] array = new int[capacity];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+int[] array = fillarray(10);
+Console.WriteLine();
+
+
+double sum = 0;
+int i = 0;
+int x = 0;
+while (i < array.Length)
+{
+    if (array[i]>0)
+    {
+        sum = sum + array[i];
+        i++;
+        x++;
+    }
+    else{i++;}
+}
+Console.WriteLine(sum/x);
---
+++ examples/proba/Program.cs
@@ -0,0 +1,16 @@
+﻿            Console.WriteLine("Чисел в массиве:  ");
+            int x = int.Parse(Console.ReadLine());
+            int[] array = new int[x];
+            int sum = 0;
+            Random rnd = new Random();
+            Console.Write("Mасив: ");
+            for (int i = 0; i < x; i++)
+            {
+                array[i] = rnd.Next(-50, 51);//заполняем массив случайными числами
+                Console.Write(array[i] + " ");
+                if (array[i] > 0)
+                {
+                    sum += array[i];
+                }
+            }
+            Console.WriteLine("Сумма = " + sum);
