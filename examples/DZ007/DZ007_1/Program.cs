42d1a0e8344f5090b84848fd4932a3135a38d220
ilovehleb <ilovehleb@gmail.com>
2022-08-25 11:20:56 +0300

    первое ДЗ седьмого семинара

--- examples/DZ007/DZ007_1/Program.cs
+++ examples/DZ007/DZ007_1/Program.cs
@@ -5,3 +5,26 @@
 // 0,5  7    -2    -0,2
 // 1    -3,3  8    -9,9
 // 8    7,8   -7,1 9
+
+
+void Array(int m, int n)
+{
+    double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11) + new Random().NextDouble();
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите размер матрицы 'm' -  ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Введите размер матрицы 'n' -  ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Array(m, n);
