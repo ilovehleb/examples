901db35d9094cc991d8ca0ab72207dbd3b27496a
ilovehleb <ilovehleb@gmail.com>
2022-08-26 21:27:30 +0300

    задача на самом семинаре 7_1

---
+++ examples/Lesson/lesson007/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+
+int[,] Generate2dArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+PrintArray(Generate2dArray(3, 4));
