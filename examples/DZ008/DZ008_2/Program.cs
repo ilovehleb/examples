efdbebce242da8a44412c2259d2a54e57b2b6cc9
ilovehleb <ilovehleb@gmail.com>
2022-08-30 09:45:15 +0300

    Второе ДЗ восьмого семинара

---
+++ examples/DZ008/DZ008_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Введите размер матрицы 'x' -  ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Введите размер матрицы 'y' -  ");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int[,] array = new int[x, y];
+
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+void MinString(int[,] array)
+{
+    Console.WriteLine();
+    int count = 0;
+    double min = 100;
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < min)
+        {
+            min = sum;
+            count = i;
+            sum = 0;
+        }
+    }
+    System.Console.WriteLine("Строка с наименьшей суммой чисел в ней - " + (count + 1));
+
+}
+FillArray(array);
+MinString(array);
