d712a8a2a8f90a2f085937034964298557292759
ilovehleb <ilovehleb@gmail.com>
2022-09-02 14:15:49 +0300

    четвертое ДЗ восьмого семинара

---
+++ examples/DZ008/DZ008_4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+//
+// 66(0,0,0) 25(0,0,1)
+// 34(0,1,0) 41(0,1,1)
+// 27(1,0,0) 90(1,0,1)
+// 26(1,1,0) 55(1,1,1)
+
+int[,,] array3D = new int[2, 2, 2];
+
+int[,,] FillArray(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                array3D[i, j, k] = new Random().Next(0, 10);
+                Console.Write(array3D[i, j, k] + "(" + i + "," + j + "," + k + ") ");
+            }
+            Console.WriteLine();
+        }
+    }
+    return array3D;
+}
+FillArray(array3D);
